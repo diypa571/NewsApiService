aaf770fd5c4d641044b451ec8c699e9ec6a76dfa
Diyar Parwana <diypa571@gmail.com>
2022-05-16 23:13:09 +0200

    Add files via upload

---
+++ models/_core.php
@@ -0,0 +1,23 @@
+<?php
+// Creating a class
+class TheDb{
+
+	// Private memebers
+    private $host  = 'localhost';
+  private $user  = '********';
+  private $password   = "********";
+  private $database  = "********";
+
+		// Creating a function for connecting to the database
+    public function getConn(){
+		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+		if($conn->connect_error){
+			die("Error- db connection " . $conn->connect_error);
+		} else {
+			// if no error return conn
+			return $conn;
+		}
+    }
+}
+?>
+
