6af67686aa73e7587e19caf03053901cec95076d
Diyar Parwana <diypa571@gmail.com>
2022-05-16 23:26:53 +0200

    Update functions.php

--- models/functions.php
+++ models/functions.php
@@ -1,5 +1,22 @@
 <?php
 
+class Article {
+    private $newstb = "news";
+    public $id;
+    public $title;
+    public $summary;
+    public $content;
+    public $author;
+    public $image;
+	  public $date;
+    private $conn;
+    
+  // The default constructor will initiate the database connection
+  // The constructor has one parameter, that will be used for the connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
 
 
+}
 ?>
